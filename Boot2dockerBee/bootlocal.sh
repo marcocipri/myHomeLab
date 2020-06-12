991443120aa4d276c9a97af6461ec82b3d1750a6
marco cipri <marcocipri@gmail.com>
2020-06-12 18:45:02 +0200

    add python

--- Boot2dockerBee/bootlocal.sh
+++ Boot2dockerBee/bootlocal.sh
@@ -1,5 +1,10 @@
+## python needed for ansible support
+tce-load -wi python
+curl https://bootstrap.pypa.io/get-pip.py | sudo python -
+## my working user
 sudo adduser bingousr
 echo "bingousr:bingousr" | sudo chpasswd
+## add my key for authentication
 mkdir -p /home/bingousr/.ssh
 cat /var/lib/boot2docker/authorized_keys >> /home/bingousr/.ssh/authorized_keys
 cat /var/lib/boot2docker/authorized_keys >> /home/docker/.ssh/authorized_keys
