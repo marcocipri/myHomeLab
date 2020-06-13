9f83449a2e870ba875dbf40114a9923941aeab2c
marco cipri <marcocipri@gmail.com>
2020-06-13 19:49:52 +0200

    cp instead mv, once the file is moved doesn't exist anymore in the dir

--- Boot2dockerBee/bootlocal.sh
+++ Boot2dockerBee/bootlocal.sh
@@ -7,4 +7,4 @@ cat /var/lib/boot2docker/authorized_keys >> /home/bingousr/.ssh/authorized_keys
 cat /var/lib/boot2docker/authorized_keys >> /home/docker/.ssh/authorized_keys
 cat /var/lib/boot2docker/sudoers-append >> /etc/sudoers
 addgroup bingousr staff
-mv /var/lib/boot2docker/install-python.sh /etc/profile.d/
+sudo cp /var/lib/boot2docker/install-python.sh /etc/profile.d/
