14bc1f930d5e7cd7899dd0e12bfe60d36cdcd999
marco cipri <marcocipri@gmail.com>
2020-06-14 11:33:47 +0200

    introducing interface conf by cli

--- Boot2dockerBee/createBoot2dockerBee.sh
+++ Boot2dockerBee/createBoot2dockerBee.sh
@@ -8,7 +8,7 @@ docker-machine scp   install-python.sh $1:/var/lib/boot2docker
 docker-machine scp   sudoers-append $1:/var/lib/boot2docker
 docker-machine stop $1
 ## when the machine is down add the bridge interface
-VBoxManage modifyvm  $1  --bridgeadapter3 "eno1"
+VBoxManage modifyvm  $1  --bridgeadapter3 "$2"
 VBoxManage modifyvm  $1  --nic3 bridged
 ## then start
 docker-machine start $1
